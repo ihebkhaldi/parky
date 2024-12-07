1b4944f1051fcc4c2ad06ec3974302e457867d23
ihebkhaldi <kiheb001@gmail.com>
2024-12-07 14:44:23 +0100

    first interface

---
+++ src/callbacks.c
@@ -0,0 +1,34 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_radioiheb_h_toggled                 (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_radioiheb_f_toggled                 (GtkToggleButton *togglebutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ajouter_iheb_clicked                (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ src/callbacks.h
@@ -0,0 +1,14 @@
+#include <gtk/gtk.h>
+
+
+void
+on_radioiheb_h_toggled                 (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radioiheb_f_toggled                 (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_ajouter_iheb_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
